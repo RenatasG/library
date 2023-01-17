4d0ae13709ba1bb886d0f81821e69fa3538e2b2f
Kleine <renatasgal420@gmail.com>
2023-01-17 13:49:11 +0200

    read server config from .env

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
+const port = process.env.PORT || 4000;
+dotenv.config();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
   }),
 );
 
-const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Created server listening on port ${port}`);
+  console.log(`Created server on port ${port}`);
 });
