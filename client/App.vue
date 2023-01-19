121f1143bbada37e6c8d26033d6367cd23a69e97
Kleine <renatasgal420@gmail.com>
2023-01-19 20:26:31 +0200

    add navbar and footer skeletons

--- client/App.vue
+++ client/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppNavbar from '@/components/AppNavbar.vue';
+import AppFooter from '@/components/AppFooter.vue';
+</script>
 
 <template>
-  <div>
+  <div class="bg-primary-500 h-screen w-screen">
+    <AppNavbar />
     <RouterView />
+    <AppFooter />
   </div>
 </template>
---
+++ client/components/AppNavbar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-60 border-r-2 h-screen">
+    <div>Bookarc</div>
+    <div>Home</div>
+    <div>Books</div>
+  </div>
+</template>
