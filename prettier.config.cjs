0ece07c41b682c3676ef8d3535e98ceb3b8cbca6
Kleine <renatasgal420@gmail.com>
2023-02-01 17:09:10 +0200

    install tailwind prettier plugin

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  singleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
