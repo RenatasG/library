51570756479e6380736b14d9c82b2ddf41ad72b2
Kleine <renatasgal420@gmail.com>
2023-02-09 16:38:16 +0200

    dynamically load books show component

--- client/router/books.ts
+++ client/router/books.ts
@@ -1,12 +1,10 @@
-import type { RouteRecordRaw } from 'vue-router';
 import type { Route } from '@/types/routes';
-import BooksShow from '@/views/BooksShow.vue';
 
 const bookRoutes = [
   {
     path: '/books/:id',
     name: 'BooksShow',
-    component: BooksShow,
+    component: () => import('@/views/BooksShow.vue'),
   },
 ] satisfies Route[];
 
