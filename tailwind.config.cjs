839ec84c136f8b01032a7142eb4a9edfb7d059e1
Kleine <renatasgal420@gmail.com>
2023-02-10 13:01:49 +0200

    chore: add more primary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         primary: {
+          100: '#F7EBEB',
           500: '#EAC7C7',
+          600: '#D99B9B',
+          700: '#C86E6E',
+          900: '#BF5757',
         },
         secondary: {
           500: '#A0C3D2',
