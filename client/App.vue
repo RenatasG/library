bc7bcd17cc2eb27d4f77c6ad76b3f38a20df6770
Kleine <renatasgal420@gmail.com>
2023-02-13 15:41:15 +0200

    feat: dynamically collapse description based on line count

--- client/App.vue
+++ client/App.vue
@@ -12,7 +12,7 @@ import AppNavbarTop from '@/components/AppNavbarTop.vue';
       <AppNavbarTop />
 
       <div
-        class="mr-4 mb-4 flex flex-grow flex-col overflow-y-auto rounded-3xl bg-white/60 p-10"
+        class="mr-4 mb-4 flex flex-grow flex-col overflow-y-auto rounded-3xl bg-primary-100 p-10"
       >
         <main class="h-full">
           <RouterView />
--- client/components/BookDescription.vue
+++ client/components/BookDescription.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
-// const collapsed = ref(false);
+const MAX_LINES = 7;
+const collapsed = ref(true);
+const tooLong = ref(false);
+
+function toggle() {
+  collapsed.value = !collapsed.value;
+}
+
+const p = ref<HTMLParagraphElement | null>(null);
+
+onMounted(() => {
+  const lineHeight = parseInt(window.getComputedStyle(p.value!).lineHeight);
+  const lines = Math.ceil(p.value!.offsetHeight / lineHeight);
+
+  if (lines > MAX_LINES) {
+    tooLong.value = true;
+  }
+});
 </script>
 
 <template>
   <div class="relative text-gray-700">
     <div
-      class="absolute flex h-full w-3 cursor-pointer items-center justify-center hover:text-primary-500/80"
+      v-if="tooLong"
+      class="absolute -left-3 flex h-full w-3 cursor-pointer items-center justify-center hover:text-primary-500"
+      @click="toggle"
     >
-      <div class="h-[97%] border-r-[1.5px] border-r-current"></div>
+      <div class="h-[97%] border-r-[0.5px] border-r-current"></div>
     </div>
 
-    <p class="ml-3">
+    <p ref="p" :class="{ 'line-clamp-7': tooLong && collapsed }">
       <slot />
     </p>
   </div>
