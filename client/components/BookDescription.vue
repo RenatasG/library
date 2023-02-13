b192317a4f8012f43fa68862cf17a6165bf864b0
Kleine <renatasgal420@gmail.com>
2023-02-13 21:59:32 +0200

    chore: toggle description on gradient click

--- client/components/BookDescription.vue
+++ client/components/BookDescription.vue
@@ -23,26 +23,34 @@ onMounted(() => {
   <div class="relative text-gray-700">
     <div
       v-if="tooLong"
-      class="absolute -left-3 flex h-full w-3 cursor-pointer items-center justify-center hover:text-primary-500"
+      class="absolute -left-3 flex h-full w-3 cursor-pointer items-center justify-center text-primary-600 hover:text-primary-500"
       @click="toggle"
     >
       <div class="h-[97%] border-r-[0.5px] border-r-current"></div>
     </div>
 
-    <p ref="p" :class="{ 'line-clamp-7 relative': tooLong && collapsed }">
+    <p
+      ref="p"
+      :class="{ 'line-clamp-7 relative': tooLong && collapsed }"
+      @click="toggle"
+    >
       <slot />
     </p>
   </div>
 </template>
 
 <style scoped>
+p {
+  pointer-events: none;
+}
 p.line-clamp-7::before {
   --line-height: 1.5;
 
   content: '';
   position: absolute;
   bottom: 0;
-  pointer-events: none;
+  pointer-events: all;
+  cursor: pointer;
   width: 100%;
   height: calc(1em * var(--line-height));
   background: linear-gradient(to bottom, transparent, #f7ebeb);
